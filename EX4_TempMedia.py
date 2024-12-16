6cfd99ce3c960324f4d8329c3485dd06834b2259
DRAS1109 <25049@esenviseu.net>
2024-12-16 14:11:15 +0000

    16/12/2024

--- EX4_TempMedia.py
+++ EX4_TempMedia.py
@@ -5,9 +5,10 @@ Meses = 12
 
 Maior = -99
 Menor = 99
+Maior_Moda = 0
 
-Temp = np.zeros(Meses) #Definir array para a tem dos meses
-Moda = np.zeros(Meses) #Definir array para a tem dos meses
+Temp = np.zeros(Meses) #Definir array para a temp dos meses
+Moda = np.zeros(Meses) #Definir array para a moda
 
 
 #Ler notas
@@ -19,25 +20,35 @@ for i in range (Meses):
 Media = Soma / Meses # Calcular a média
 print (f"A média do ano: {Media}")
 
-for N in range (Meses): #Percorrer as Temp. e verificar a maior e a menor
-    if Temp[N] < Maior:
-        Maior = Temp[N]
-        N_Maior = N
+for Pos in range (Meses): #Percorrer as Temp. e verificar a maior e a menor
+    if Temp[Pos] > Maior:
+        Maior = Temp[Pos]
+        N_Maior = Pos + 1
 
-    if Temp[N] > Menor:
-        Menor = Temp[N]
-        N_Menor = N
+    if Temp[Pos] < Menor:
+        Menor = Temp[Pos]
+        N_Menor = Pos + 1
 
-print (f"A maior média mensal foi {Maior}, do {N_Maior + 1}º mês")
-print (f"A menor média mensal foi {Menor}, do {N_Menor + 1}º mês")
+print (f"A maior média mensal foi {Maior}, do {N_Maior}º mês")
+print (f"A menor média mensal foi {Menor}, do {N_Menor}º mês")
 
 
-for N in range (Meses): #Percorrer as Temp. e verificar a inferior e superior à média
-    if Temp[N] < Media:
-        print(f"A media do {N + 1}º mês é inferior à média.")
+for Pos in range (Meses): #Percorrer as Temp. e verificar a inferior e superior à média
+    if Temp[Pos] < Media:
+        print(f"A media do {Pos + 1}º mês é inferior à média.")
     
-    if Temp[N] > Media:
-        print(f"A media do {N + 1}º mês é superior à média.")
+    if Temp[Pos] > Media:
+        print(f"A media do {Pos + 1}º mês é superior à média.")
 
-for N in range (Meses):
-    if Temp[N]
+
+for Pos in range (Meses): #Percorrer as Temp.
+    for N in Temp:
+        if Temp[Pos] == N:
+            Moda[Pos] = Moda[Pos] + 1
+
+for Pos in range (len(Moda)): #Identifica a moda
+    if Moda[Pos] > Maior_Moda:
+        Maior_Moda = Moda[Pos]
+        Qual_Moda = Temp[Pos]
+
+print (f"A moda é {Qual_Moda} e apareceu {Maior_Moda} vezes")
