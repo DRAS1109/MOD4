309f39aedc3b9d8c01c18baf044714e5fa44ce73
DRAS1109 <25049@esenviseu.net>
2025-02-03 16:35:48 +0000

    03/02/2025

--- Ex19_VendingMachine.py
+++ Ex19_VendingMachine.py
@@ -1,10 +1,17 @@
 """
-O cliente insere o dinheiro para comprar (5€) e o preco do produto(3€) e depois devolve o troco (2€)
+O cliente insere o dinheiro para comprar (5€), o preco do produto(3€) e depois devolve o troco (2€)
 """
 
 import numpy as np
 
-Disponivel = np.array([[2 , 10], [1 , 10] , [0.5 , 10] , [0.2 , 10] , [0.1 , 10] , [0.05 , 10] , [0.02 , 10] , [0.01 , 1000]])
+Disponivel = np.array([[2   , 10], 
+                       [1   , 10], 
+                       [0.5 , 10], 
+                       [0.2 , 10], 
+                       [0.1 , 10],
+                       [0.05 , 10],
+                       [0.02 , 10], 
+                       [0.01 , 1000]])
 
 
 Preço = float(input("Quanto custa o produto: "))
@@ -17,8 +24,8 @@ if Troco < 0:
 
 
 for i in range(Disponivel.shape[1]):
-    Moedas = 0
-    if Troco - Disponivel[i, 0] > 0:
+    while Troco - Disponivel[i, 0] > 0:
+        Moedas = 0
         Troco = Troco - Disponivel[i, 0]
         Moedas = Moedas + 1
     print(f"Troco: {Moedas} moedas de {Disponivel[i, 0]}€ ")
