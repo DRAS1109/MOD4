05166b6a68a4f69345c69f3726bb8c3f46e35c89
DRAS1109 <25049@esenviseu.net>
2025-02-04 08:55:47 +0000

    04/02/2025

--- Ex19_VendingMachine.py
+++ Ex19_VendingMachine.py
@@ -4,14 +4,14 @@ O cliente insere o dinheiro para comprar (5€), o preco do produto(3€) e depo
 
 import numpy as np
 
-Disponivel = np.array([[2   , 10], 
-                       [1   , 10], 
-                       [0.5 , 10], 
-                       [0.2 , 10], 
-                       [0.1 , 10],
-                       [0.05 , 10],
-                       [0.02 , 10], 
-                       [0.01 , 1000]])
+Disponivel = np.array([[2    , 1], 
+                       [1    , 1], 
+                       [0.5  , 1], 
+                       [0.2  , 1], 
+                       [0.1  , 1],
+                       [0.05 , 1],
+                       [0.02 , 1], 
+                       [0.01 , 1]])
 
 
 Preço = float(input("Quanto custa o produto: "))
@@ -23,9 +23,19 @@ if Troco < 0:
     print("O dinheiro inserido não chega para comprar o produto")
 
 
-for i in range(Disponivel.shape[1]):
-    while Troco - Disponivel[i, 0] > 0:
-        Moedas = 0
+for i in range(Disponivel.shape[0]):
+    Moedas = 0
+    while Troco - Disponivel[i, 0] >= 0 and Disponivel[i,1] > 0:
         Troco = Troco - Disponivel[i, 0]
+        Troco = round(Troco, 2)
         Moedas = Moedas + 1
-    print(f"Troco: {Moedas} moedas de {Disponivel[i, 0]}€ ")
+    
+        Disponivel[i,1] = Disponivel[i,1] - 1
+
+    if Moedas != 0:
+        print(f"Troco: {Moedas} moedas de {Disponivel[i, 0]}€ ")
+
+if Troco > 0:
+    print("Não tenho moedas suficientes para completar o troco.")
+
+print(Disponivel)
